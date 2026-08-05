4f6046b08e38e5a4efafda40cc11e35a5bfd4dba
Enzo Durel <enzo.durel@gmail.com>
2026-08-05 13:40:39 +0200

    better create vm handle

--- app/main.py
+++ app/main.py
@@ -73,11 +73,7 @@ async def add_config(websocket: WebSocket,
         await websocket.send_text(f"[ADD] Starting registring VM '{vm_id}:{team_name}'...")
 
         # Create vm config in the database
-        try:
-            create_vm(team_name, vm_id, vm_ip, student_emails)
-        except ValueError as ve:
-            logging.info(f"[ADD] Error when registring the vm: {str(ve)}")
-            raise
+        create_vm(team_name, vm_id, vm_ip, student_emails)
 
         await websocket.send_text(f"[ADD] Successfully Created VM in DB.")
 
@@ -97,6 +93,9 @@ async def add_config(websocket: WebSocket,
         await websocket.send_text(f"[ADD] Successfully Register VM to Guacamole.")
     except WebSocketDisconnect:
         logging.info("Client disconnected during deployment execution.")
+    except ValueError as ve:
+        await websocket.send_text(f"[ADD] Error when registring the vm: {str(ve)}")
+        logging.error(f"[ADD] Error when registring the vm: {str(ve)}")
     finally:
         try:
             await websocket.close()
